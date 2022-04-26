996804915ff805a38545b3748741d467a5a04e6c
Vladyslav Martynenko <vladyslav.martynenko@smartteksas.com>
2022-04-26 11:14:42 +0200

    add check for 401 error

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+import { HttpInterceptor, HttpRequest, HttpHandler,HttpErrorResponse } from "@angular/common/http";
 import { TokenService } from "../shared/token.service";
+import {tap} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private tokenService: TokenService) { }
+
+  constructor(private tokenService: TokenService,private router: Router) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const accessToken = this.tokenService.getToken();
     req = req.clone({
@@ -11,6 +16,15 @@ export class AuthInterceptor implements HttpInterceptor {
         Authorization: "Bearer " + accessToken
       }
     });
-    return next.handle(req);
+
+    return next.handle(req).pipe( tap(() => {},
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 401) {
+            return;
+          }
+          this.router.navigate(['login']);
+        }
+      }));
   }
 }
